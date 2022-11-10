880b24b74c7adaba35a6a77cef2a03caf4e11e0c
Guadalupe Gop <g.gop112019@gmail.com>
2022-11-10 11:17:09 -0600

    Manipulacion de DOM

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -6,13 +6,16 @@ function seleccionarPeleadorJugador() {
   let inputCoku = document.getElementById("coku");
   let inputVegeto = document.getElementById("vegeto");
   let inputMarioBuu = document.getElementById("mariobuu");
-
+  let jugadorParticipante = document.getElementById("participante-judagor");
   if (inputCoku.checked) {
     alert("Seleccionaste a Coku");
+    jugadorParticipante.innerHTML = "Coku";
   } else if (inputVegeto.checked) {
     alert("Seleccionaste a Vegeto");
+    jugadorParticipante.innerHTML = "Vegeto";
   } else if (inputMarioBuu.checked) {
     alert("Seleccionaste a Mario Buu");
+    jugadorParticipante.innerHTML = "Mario Buu";
   } else {
     alert("Selecciona una Opción");
   }
